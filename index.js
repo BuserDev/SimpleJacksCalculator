9ec08839cacb343d915fe747b3aca6516fc48098
Robert Buser <supplyguyusmc@yahoo.com>
2015-04-27 16:48:51 -0700

    Simple Jack web app

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
+var http = require('http');
+var httpserver = http.Server(app);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(cool());
+  response.sendfile(__dirname + '/calculator.html');
 });
 
 app.listen(app.get('port'), function() {
